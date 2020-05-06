6a5756d9c13e38f8e734407445f31f0ec064dca7
WAI-f <2427697582@qq.com>
2020-05-07 01:56:19 +0800

    basic-introduction

--- basic/introduction/cpp/operation_test.cpp
+++ basic/introduction/cpp/operation_test.cpp
@@ -17,5 +17,18 @@ int main()
 	y.add_(x);
 	std::cout << y << std::endl;
 
+	// Resizing: If you want to resize / reshape tensor :
+	torch::Tensor m = torch::randn({ 4, 4 });
+	torch::Tensor n = m.view(16);
+	torch::Tensor k = m.view({ -1,8 });
+	std::cout << m.sizes() << std::endl;
+	std::cout << n.sizes() << std::endl;
+	std::cout << k.sizes() << std::endl;
+
+	// one element tensor, use .item() to get the value
+	torch::Tensor r = torch::randn(1);
+	std::cout << r << std::endl;
+	std::cout << r.item() << std::endl;
+
 	return 0;
 }
